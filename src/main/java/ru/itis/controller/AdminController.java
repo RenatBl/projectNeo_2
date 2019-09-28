8439af77ad035a506637d66a962855a4a46a1cf1
Renat <renat.blinov@yandex.ru>
2019-09-28 22:48:17 +0300

    Added admin controller

---
+++ src/main/java/ru/itis/controller/AdminController.java
@@ -0,0 +1,22 @@
+package ru.itis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.itis.repository.UserRepository;
+
+@Controller
+@PreAuthorize("hasAuthority('ADMIN')")
+public class AdminController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+
+        return "users";
+    }
+}
