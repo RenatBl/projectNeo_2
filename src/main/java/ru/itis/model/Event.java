96163c66f27097e7ce568bc1cfe163184857cce3
Renat <renat.blinov@yandex.ru>
2019-09-28 22:24:26 +0300

    Added event entity

--- src/main/java/ru/itis/model/Event.java
+++ src/main/java/ru/itis/model/Event.java
@@ -1,4 +1,27 @@
 package ru.itis.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Data
+@Table(name = "event")
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String tag;
+    private String text;
+    private Date deadline;
+    private Long target;
+    private Long picking;   //сколько собрано на данный момент
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
 }
